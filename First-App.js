f004b6839fc7d2856ee6051506039ea1a5a042e3
aashishkumar99info <aashishkumar99info@gmail.com>
2023-05-21 21:21:38 +0530

    Added a hard coded object creations in js

--- First-App.js
+++ First-App.js
@@ -1,1 +1,21 @@
 console.log("Hello World.....!")
+
+// Example of the way of creating object in JS using hard coding the properties and value pairs 
+// and a property can also be a method or function also like here is greet.
+const student = {
+
+    name: "King",
+
+    age: 14,
+
+    greet() {
+
+        console.log("Hi, I am " + this.name);
+
+    }
+
+}
+
+console.log(student)
+
+student.greet()
