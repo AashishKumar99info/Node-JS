27325ff3ca5327f54ac0c07dab33b9164eefbe91
AashishKumar99info <125687064+AashishKumar99info@users.noreply.github.com>
2023-07-17 06:19:07 +0000

    Creating A Demo Server in Node js

---
+++ App.js
@@ -0,0 +1,28 @@
+console.log("Hello World");
+
+// let's Create a Server in Node Js. To do so see the code below
+
+const http = require('http');
+
+function rqListner(req,res) {
+    console.log(req)
+}
+
+const server = http.createServer(rqListner);  
+
+server.listen(3000)
+
+// we can do this alos like this
+
+
+// http.createServer(function (req,res){        
+    // node will exectute this function for each sent request to our server.
+    // this is a event driven architecture
+    
+// }); 
+
+//OR
+
+// http.createServer((req,res)=>{
+        //it is also same as above.
+//});
